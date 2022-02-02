7c7574f6b00f8a36f6bd92f74104f22e4902d7f7
Win10 <Win10@DESKTOP-VBO5PD9>
2022-02-02 22:20:34 +0700

    lab7 na kub

---
+++ LAB7/src/Lab7/Main.java
@@ -0,0 +1,16 @@
+package Lab7 ;
+
+public class Main {
+		Stack A = new Stack();
+		int Answer = 0;
+		A.Push(15);
+		A.Push(5);
+		A.Push(8);
+		Answer = A.Top() + A.pop() + A.isEmpty() ;
+		
+		
+	
+
+
+
+}
---
+++ LAB7/src/Lab7/SuperCar.java
@@ -0,0 +1,11 @@
+package Lab7;
+
+public class SuperCar extends car {
+	car 	C = new car();
+	int x ;
+
+	public void Move() {
+		x = C.Wheel ;
+		System.out.println ("SuperCar Move VRANNNNNNNNNNNNNN!!!");
+	}
+}
